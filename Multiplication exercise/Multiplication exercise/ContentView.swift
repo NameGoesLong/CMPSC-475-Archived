b51917c1558ce3c6848ad9dbca48dfa0853921fc
Chenyin Zhang <cbz5089@psu.edu>
2020-08-31 19:09:51 -0400

    Split the QuestView.

--- Multiplication exercise/Multiplication exercise/ContentView.swift
+++ Multiplication exercise/Multiplication exercise/ContentView.swift
@@ -11,10 +11,14 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack(alignment: .center){
-            TitleView().padding(.vertical)
+            Spacer()
+            TitleView()
+            Spacer()
             MainView()
                 .padding(.vertical)
-        }.frame( maxWidth: .infinity, maxHeight: .infinity).background(Color.orange)
+            Spacer()
+        }.frame( maxWidth: .infinity, maxHeight: .infinity).background(Color.yellow.opacity(0.4)).foregroundColor(Color.pink.opacity(0.6))
+        
     }
 }
 
@@ -35,22 +39,25 @@ struct TitleView: View {
 }
 
 struct MainView: View {
+    let selection_arr : [Int] = [175, 179, 176, 181]
     var body: some View{
         VStack{
             ScoreView()
                 .padding(.vertical)
-            QuestView().padding(.vertical)
+            QuestView().padding(.top)
+            ChoiceView(choices: self.selection_arr)
         }
     }
 }
 
 
 struct ScoreView: View {
-    let correctCounter : Int = 2
+    let correctCounter : Int = 1
+    let score : [Int] = [1,2,0,0,0]
     var body: some View{
         VStack{
             HStack{
-                ForEach(1..<6){i in
+                ForEach(score, id: \.self){i in
                     IndicatorView(correctness: i)
                 }.padding(2.0).background(Circle().fill(Color.gray).opacity(0.4))
             }
@@ -62,17 +69,11 @@ struct ScoreView: View {
     }
 }
 
+//View for showing the question
 struct QuestView: View {
     let problemId : Int = 3
-    let multiplicand : Int
-    let multiplier : Int
-    let result : Int
-    
-    init(){
-        multiplicand = Int.random(in: 1...15)
-        multiplier = Int.random(in: 1...15)
-        result = multiplier * multiplicand
-    }
+    let multiplicand : Int = 17
+    let multiplier : Int = 8
     
     var body: some View{
         VStack{
@@ -82,15 +83,24 @@ struct QuestView: View {
                 Text("X \(multiplier)")
             }.padding(.top).font(.largeTitle)
             Rectangle().frame(width: 100.0, height: 7.0)
-            HStack{
-                ForEach(1..<5){i in
-                    Button("\(self.result + i - 1)"){}
-                }.padding(.all, 10.0).background(RoundedRectangle(cornerRadius: 15).fill(Color.blue).opacity(0.5))
-            }.padding(.all)
         }
     }
 }
 
+//View for button selection
+struct ChoiceView: View{
+    let choices : [Int]
+    var body: some View{
+        HStack{
+            ForEach(choices, id: \.self){ choice in
+                Button("\(choice)"){}.padding(.all, 10.0).background(RoundedRectangle(cornerRadius: 15).fill(Color.blue).opacity(0.5)).foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
+            }
+            .padding(.top)
+        }
+    }
+}
+
+//View for indication of correctness
 struct IndicatorView: View {
     let correctness: Int
     var body: some View{
@@ -106,7 +116,8 @@ struct IndicatorView: View {
     }
 }
 
-struct JugdgeView: View{
+//Not used in HW1
+struct JudgeView: View{
     let result : Int
     var body: some View{
         VStack{
