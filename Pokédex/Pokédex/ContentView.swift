78f1acaca53d6f68044ba11213497c49199cde5a
Chenyin Zhang <cbz5089@psu.edu>
2020-09-20 17:30:40 -0400

    Created the new project.

---
+++ Pokédex/Pokédex/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pokédex
+//
+//  Created by New User on 20/9/20.
+//  Copyright © 2020 Chenyin Zhang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
