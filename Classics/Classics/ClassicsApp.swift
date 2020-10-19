d36a002a4033bd5aec7ce6cbab898d912cf4a47b
Chenyin Zhang <cbz5089@psu.edu>
2020-10-18 22:42:30 -0400

    Creating the new project.

---
+++ Classics/Classics/ClassicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassicsApp.swift
+//  Classics
+//
+//  Created by New User on 18/10/20.
+//
+
+import SwiftUI
+
+@main
+struct ClassicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
